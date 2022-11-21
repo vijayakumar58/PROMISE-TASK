3a3124548c0586d47b8c2a54612ef4c4d6bfe9c7
vijayakumar58 <vijayalingam58@gmail.com>
2022-11-21 15:44:50 +0530

    add js

---
+++ script.js
@@ -0,0 +1,26 @@
+let div=document.createElement("div");
+div.setAttribute("id","division");
+let input=document.createElement("input");
+input.setAttribute("type","text")
+input.setAttribute("id","in");
+let but=document.createElement("button");
+but.setAttribute("type","button");
+but.innerText="search";
+div.append(input);
+div.append(but);
+document.body.append(div);
+
+const covid= new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+    request.open("GET","https://coronavirus.m.pipedream.net/");
+    request.send();
+    request.onload=function(){
+       if (request.status==200){
+        let data= JSON.parse(request.response)
+        resolve(data)
+       }else{
+        reject("some error not found")
+       }
+    }
+})
+covid.then((res)=>{console.log(res)}).catch((rej)=>{console.log(rej)})
